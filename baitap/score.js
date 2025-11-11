ef0f408e1750cbdbb5f4b8ba1d92567a3108dccf
sytuan2001 <thuancat1978@gmail.com>
2025-11-11 15:47:10 +0700

    Add

--- baitap/score.js
+++ baitap/score.js
@@ -11,3 +11,31 @@ function tinhDiem() {
         "Tổng điểm: " + tong + "<br>" +
         "Điểm trung bình: " + trungBinh.toFixed(2);
 }
+// Nhiệt độ
+function chuyenDoi() {
+    let doC = +document.getElementById("c").value;
+    let doF = (9*doC / 5) + 32;
+    document.getElementById("result").innerHTML = "Result: " + doF;
+}
+// Diện tích hình tròn
+function tinhDienTich() {
+    var r = parseFloat(document.getElementById('bankinh1').value);
+
+    // Công thức: Diện tích = π × r²
+    var dienTich = Math.PI * r * r;
+
+    document.getElementById('ketqua1').innerHTML =
+        "Bán kính: " + r + "<br>" +
+        "Diện tích hình tròn: " + dienTich.toFixed(2);
+}
+// Chu vi hình tròn
+function tinhChuVi() {
+    var r = parseFloat(document.getElementById('bankinh2').value);
+
+    // Công thức: Chu vi = 2 × π × r
+    var chuVi = 2 * Math.PI * r;
+
+    document.getElementById('ketqua2').innerHTML =
+        "Bán kính: " + r + "<br>" +
+        "Chu vi hình tròn: " + chuVi.toFixed(2);
+}
